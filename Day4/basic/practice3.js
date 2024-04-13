2a965e350bf96872fa48e35aebce0a73aa610c52
Shivansh-12345 <147116452+Shivansh-12345@users.noreply.github.com>
2024-04-13 09:48:49 +0530

    Add files via upload

---
+++ Day4/basic/practice3.js
@@ -0,0 +1,11 @@
+let newBtn=document.createElement("button");
+newBtn.innerText="click me";
+
+newBtn.style.color="white";
+newBtn.style.background="red";
+
+document.querySelector("body").prepend(newBtn)
+
+
+//q2
+let para=do
--- Day4/basic/script3.js
+++ Day4/basic/script3.js
@@ -1,18 +1,39 @@
 // let div=document.querySelector("div");
 // console.log(div);
 
-let v=div.getAttribute("id");
-console.log(v);
+// let v=div.getAttribute("id");
+// console.log(v);
 
-let name=div.getAttribute("name");
-console.log(name);
+// let name=div.getAttribute("name");
+// console.log(name);
 
-let para=document.querySelector("p");
-console.log(para.setAttribute("class","new class"));
+// let para=document.querySelector("p");
+// console.log(para.setAttribute("class","new class"));
 
-let div=document.querySelector("div");
+// let div=document.querySelector("div");
 
-div.style.backgroundColor="green";
-div.style.fontsize="26px";
-div.style.visibility="hidden";
-div.innerText="hello";
+// div.style.backgroundColor="green";
+// div.style.fontsize="26px";
+// div.style.visibility="hidden";
+// div.style.fontSize="26px";
+
+// div.innerText="hello";
+// let newbtn = document.createElement("button");
+// newbtn.innerText = "clickme";
+// console.log(newbtn);
+
+// let div=document.querySelector("p");
+// //div.append(newbtn);
+// //div.prepend(newbtn);
+// //div.before(newbtn);
+// div.after(newbtn);
+
+// let heading=document.createElement("h1");
+// heading.innerHTML="<h1> I'm new! </h1>";
+
+// document.querySelector("body").prepend(heading);
+
+// let para=document.querySelector("p");
+// para.remove();
+
+// heading.remove();
